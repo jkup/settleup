e20c1d65d1cfe5fb4c38a660f018dbb27a5577bc
Kelly King <kking@twitter.com>
2017-09-09 19:23:25 -0700

    components directory

--- src/components/App.js
+++ src/components/App.js
@@ -18,7 +18,7 @@ class App extends Component {
     return (
       <Container>
         <Header>
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Settle Up</h2>
         </Header>
         <Text>
           To get started, edit <code>src/App.js</code> and save to reload.
